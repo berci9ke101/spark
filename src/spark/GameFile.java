2d08a6d204afeb57c62ac309f9333a2b23bc2783
Bertalan <berci9ke101@gmail.com>
2022-11-04 18:58:27 +0100

    krankenhaus elvileg csak a main game hiányzik

---
+++ src/spark/GameFile.java
@@ -0,0 +1,89 @@
+package spark;
+
+import spark.quest.*;
+
+import java.io.*;
+
+/**
+ * A class for the game file and its manipulation
+ */
+public class GameFile
+{
+    /*Attributes*/
+    private QuestQueue queue;           //The queue for the Quests
+
+    /*Methods*/
+
+    /**
+     * Starts a new game from a given file.
+     *
+     * @param name The name of the gamefile
+     * @throws IOException If the file doesn't exist
+     */
+    public void newGame(String name) throws IOException
+    {
+        BufferedReader read = new BufferedReader(new FileReader(name));
+        queue = new QuestQueue();
+        String line;
+        while ((line = read.readLine()) != null)
+        {
+            String[] data = line.split(";");
+
+            int ID = Integer.parseInt(data[0]);
+            String desc = data[2];
+            String optA = data[3];
+            int jmpA = Integer.parseInt(data[4]);
+            String optB = data[5];
+            int jmpB = Integer.parseInt(data[6]);
+
+            switch (data[1])
+            {
+                case "B":
+                    queue.add(new BasicQuest(ID, desc, optA, optB, jmpA, jmpB));
+                    break;
+                case "V":
+                    int altjmp = Integer.parseInt(data[8]);
+                    String altdsc = data[7];
+                    queue.add(new VisitableQuest(ID, desc, optA, optB, jmpA, jmpB, altdsc, altjmp));
+                    break;
+                case "R":
+                    queue.add(new RandomQuest(ID, desc, optA, optB, jmpA, jmpB));
+                    break;
+            }
+        }
+        read.close();
+    }
+
+
+    /**
+     * Loads a savegame from a given file.
+     *
+     * @param name The file reference of the save file
+     * @throws ClassNotFoundException If the object isn't QuestQueue type
+     * @throws IOException            If the file doesn't exist
+     */
+    public void loadGame(File name) throws IOException, ClassNotFoundException
+    {
+        if (!name.getName().contains(".sprkdt"))
+        {
+            throw new IOException("Wrong file");
+        }
+        queue = new QuestQueue();
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(name));
+        queue = (QuestQueue) ois.readObject();
+        ois.close();
+    }
+
+    /**
+     * Saves out the current game to a specified file
+     *
+     * @param name The name of the file
+     * @throws IOException If the file doesn't exist
+     */
+    public void saveGame(String name) throws IOException
+    {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(name += ".sprkdt"));
+        oos.writeObject(queue);
+        oos.close();
+    }
+}
---
+++ src/spark/SparkFrame.java
@@ -0,0 +1,143 @@
+package spark;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * The framing (pun intended) of the game
+ */
+public class SparkFrame
+{
+    /*Attributes*/
+    private GameFile GAME;              //The game file of the game
+    private MainMenu mainMenu;          //The main menu of the game
+
+    /**
+     * The Main Menu of the game
+     */
+    private class MainMenu extends JFrame
+    {
+        /*Attributes*/
+
+        private JButton newGame = new JButton("New Game");   //Button for new game
+        private JButton loadGame = new JButton("Load Game"); //Button for load game
+        private newGAME newGAME = new newGAME();                                  //The New Game screen
+        private loadGAME loadGAME = new loadGAME();                                //The Load Game screeen
+
+        /*Methods*/
+        private void initComponents()
+        {
+            this.setLayout(new GridLayout(2, 1));
+
+            newGame.addActionListener(e ->
+            {
+                newGAME.setVisible(true);
+                loadGAME.setVisible(false);
+            });
+            loadGame.addActionListener(e ->
+            {
+                loadGAME.setVisible(true);
+                newGAME.setVisible(false);
+            });
+
+            this.add(newGame);
+            this.add(loadGame);
+        }
+
+
+        /**
+         * Class for the new game selection screen
+         */
+        private class newGAME extends JFrame
+        {
+            /*Attributes*/
+            JTextField filename = new JTextField(20);
+            JButton start = new JButton("New Game");
+
+            /*Methods*/
+
+            /**
+             * Constructor
+             */
+            public newGAME()
+            {
+                this.initComponents();
+            }
+
+            /**
+             * Initializes the components
+             */
+            private void initComponents()
+            {
+                this.setLayout(new BorderLayout());
+
+                JPanel panel = new JPanel();
+                panel.add(filename);
+
+                start.addActionListener(e ->
+                {
+                    try
+                    {
+                        GAME.newGame(filename.getText());
+                    } catch (IOException ex)
+                    {
+                        JOptionPane.showMessageDialog(newGame, "No such file! Try again.");
+                    }
+                });
+
+                panel.add(start);
+
+                this.add(panel, BorderLayout.CENTER);
+            }
+        }
+
+        /**
+         * Class for the load game selection screen
+         */
+        private class loadGAME extends JFrame
+        {
+            /*Attributes*/
+            private JButton load = new JButton("Load");       //Load button
+            private JComboBox options;                            //List of the available game files
+
+            /*Methods*/
+
+            /**
+             * Constructor
+             */
+            public loadGAME()
+            {
+                this.initComponents();
+            }
+
+            /**
+             * Initializes the components
+             */
+            private void initComponents()
+            {
+                this.setLayout(new BorderLayout());
+
+                JPanel panel = new JPanel();
+                File location = new File(System.getProperty("user.dir"));
+                File[] files = location.listFiles();
+                options = new JComboBox(files);
+                panel.add(options);
+
+                load.addActionListener(e ->
+                {
+                    try
+                    {
+                        GAME.loadGame((File) options.getSelectedItem());
+                    } catch (Exception ex)
+                    {
+                        JOptionPane.showMessageDialog(newGame, "Can't open file! Try again.");
+                    }
+                });
+                panel.add(load);
+                this.add(panel, BorderLayout.CENTER);
+            }
+        }
+    }
+}
