443600ae2b08840deeff531d6b857dbbea7e644a
Bertalan Kovács <berci9ke101@gmail.com>
2022-11-14 14:42:18 +0100

    doc

--- src/spark/Application.java
+++ src/spark/Application.java
@@ -2,6 +2,11 @@ package spark;
 
 public class Application
 {
+    /**
+     * Entry point
+     *
+     * @param args - Arguments
+     * */
     public static void main(String[] args)
     {
         new SparkFrame();
